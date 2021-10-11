ba63f3827eb2a57ee4d3d7e0d216db094443cfb2
Elton Chavez <elton.mct@gmail.com>
2021-10-11 01:33:19 -0300

    newUpdateBackend

--- index.js
+++ index.js
@@ -1,62 +1,65 @@
-// const http = require('http');
-// const { response } = require('express')
+require('dotenv').config()
+require('./mongo')
+
 const express = require('express')
 const cors = require('cors')
 const logger = require('./loggerMiddleware')
 const app = express()
+const Note = require('./models/Note')
+const notFound = require('./middlewares/notFound')
+const handleErrors = require('./middlewares/handleErrors')
+
 app.use(cors())
 app.use(express.json())
 app.use(logger)
-
-let notes = [
-  {
-    'id': 1,
-    'content': 'Me tengo que anotar a la facultad',
-    'date': '2019-05-30T17:30:31.098Z',
-    'important': true
-  },
-  {
-    'id': 2,
-    'content': 'Estudiar mas de backend',
-    'date': '2019-05-30T17:30:31.098Z',
-    'important': false
-  },
-  {
-    'id': 3,
-    'content': 'No detenerme nunca en mis metas',
-    'date': '2019-05-30T17:30:31.098Z',
-    'important': true
-  },
-]
-
-// const app = http.createServer((request, response) => { 
-//   response.writeHead(200, { 'Content-type': 'application/json' })
-//   response.end(JSON.stringify(notes))
-// })
+// app.use('/images', express.static('images'))
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello world</h1>')
 })
 
 app.get('/api/notes', (request, response) => {
-  response.json(notes)
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
 })
 
-app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
-  if(note) {
-    response.json(note)
-  }else {
-    response.status(404).end()
+app.get('/api/notes/:id', (request, response, next) => {
+  const id = request.params.id
+  Note.findById(id)
+    .then(note => {
+      if(note) return response.json(note)
+      response.status(404).end()
+    })
+    .catch(error => {
+      next(error)
+    })
+})
+
+app.put('/api/notes/:id', (request, response, next) => {
+  const id = request.params.id
+  const note = request.body
+
+  const newNoteInfo = {
+    content: note.content,
+    important: note.important
   }
+
+  Note.findByIdAndUpdate(id, newNoteInfo, { new: true })
+    .then(result => {
+      response.status(200).json(result)
+    })
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  console.log(id)
-  notes = notes.filter(note => note.id !== id)
-  response.status(204).end()
+app.delete('/api/notes/:id', (request, response, next) => {
+  const id = request.params.id
+  Note.findByIdAndRemove(id)
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch(error => {
+      next(error)
+    })
 })
 
 app.post('/api/notes', (request, response) => {
@@ -68,18 +71,20 @@ app.post('/api/notes', (request, response) => {
     })
   }
 
-  const ids = notes.map( note => note.id )
-  const maxId = Math.max(...ids)
-  const newNote = {
-    id: maxId + 1,
+  const newNote = new Note({
     content: note.content,
-    important: (typeof note.important !== undefined) ? note.important : false,
-    date: new Date().toISOString()
-  }
-  notes = [...notes, newNote]
+    date: new Date(),
+    important: note.important || false
+  })
 
-  response.status(201).json(newNote)
+  newNote.save().then(savedNote => {
+    response.status(201).json(savedNote)
+  })
+
 })
+
+app.use(notFound)
+app.use(handleErrors)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
---
+++ mongo.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+const connectionString = process.env.MONGO_DB_URI
+
+mongoose.connect(connectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+  .then(() => {
+    console.log('Database connected')
+  })
+  .catch(error => {
+    console.error(error)
+  })
+
+process.on('uncaughtException', () => {
+  mongoose.connection.disconnect()
+})
