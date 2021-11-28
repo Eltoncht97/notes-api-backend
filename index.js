81964ff4045ddb9b4fd1bc7a903472779b328b8d
Elton Chavez <elton.mct@gmail.com>
2021-11-28 01:09:48 -0300

    changes

--- index.js
+++ index.js
@@ -5,88 +5,37 @@ const express = require('express')
 const cors = require('cors')
 const logger = require('./loggerMiddleware')
 const app = express()
-const Note = require('./models/Note')
 const notFound = require('./middlewares/notFound')
 const handleErrors = require('./middlewares/handleErrors')
+const usersRouter = require('./controllers/users.js')
+const notesRouter = require('./controllers/notes')
+const loginRouter = require('./controllers/login')
+const testingRouter = require('./controllers/testing')
 
 app.use(cors())
 app.use(express.json())
 app.use(logger)
-// app.use('/images', express.static('images'))
+app.use('/images', express.static('images'))
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello world</h1>')
 })
 
-app.get('/api/notes', (request, response) => {
-  Note.find({}).then(notes => {
-    response.json(notes)
-  })
-})
+app.use('/api/users', usersRouter)
+app.use('/api/notes', notesRouter)
+app.use('/api/login', loginRouter)
 
-app.get('/api/notes/:id', (request, response, next) => {
-  const id = request.params.id
-  Note.findById(id)
-    .then(note => {
-      if(note) return response.json(note)
-      response.status(404).end()
-    })
-    .catch(error => {
-      next(error)
-    })
-})
-
-app.put('/api/notes/:id', (request, response, next) => {
-  const id = request.params.id
-  const note = request.body
-
-  const newNoteInfo = {
-    content: note.content,
-    important: note.important
-  }
-
-  Note.findByIdAndUpdate(id, newNoteInfo, { new: true })
-    .then(result => {
-      response.status(200).json(result)
-    })
-})
-
-app.delete('/api/notes/:id', (request, response, next) => {
-  const id = request.params.id
-  Note.findByIdAndRemove(id)
-    .then(() => {
-      response.status(204).end()
-    })
-    .catch(error => {
-      next(error)
-    })
-})
-
-app.post('/api/notes', (request, response) => {
-  const note = request.body
-
-  if(!note || !note.content){
-    return response.status(400).json({
-      error: 'note.content is missing'
-    })
-  }
-
-  const newNote = new Note({
-    content: note.content,
-    date: new Date(),
-    important: note.important || false
-  })
-
-  newNote.save().then(savedNote => {
-    response.status(201).json(savedNote)
-  })
-
-})
+if(process.env.NODE_ENV === 'test' ){
+  app.use('/api/testing', testingRouter)
+}
 
 app.use(notFound)
 app.use(handleErrors)
 
+// eslint-disable-next-line no-undef
 const PORT = process.env.PORT || 3001
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
+
+module.exports = {app, server}
--- mongo.js
+++ mongo.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
-const connectionString = process.env.MONGO_DB_URI
+const {MONGO_DB_URI, MONGO_DB_URI_TEST, NODE_ENV} = process.env
+const connectionString = NODE_ENV==='test' ? MONGO_DB_URI_TEST : MONGO_DB_URI
 
 mongoose.connect(connectionString, {
   useNewUrlParser: true,
@@ -12,6 +13,7 @@ mongoose.connect(connectionString, {
     console.error(error)
   })
 
-process.on('uncaughtException', () => {
+process.on('uncaughtException', error => {
+  console.log(error)
   mongoose.connection.disconnect()
 })
